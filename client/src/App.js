40f317301096fdb3f24aa6fd68a8c1b01912a2fa
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-10-15 00:03:16 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
 import React, { lazy, Suspense } from "react";
-import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary";
-
-const Blog = lazy(() => import("./components/Blog"));
+import Blog from './components/Blog';
 const Comments = lazy(() => import("./components/Comments"));
-
-
 
 function App() {
   return (
     <ErrorBoundary>
-      <Suspense fallback={<h4 style={{textAlign: 'center'}}>Loading...</h4>}>
-        <Blog />
-      </Suspense>
+      <Blog />
       <Suspense fallback={<h4 style={{textAlign: 'center'}}>Loading...</h4>}>
         <Comments />
       </Suspense>
